6f2853741d8ce5c500eeb3ffea9e6d485c90f1f8
ld <ld@hackerdom.ru>
2012-10-11 00:15:15 +0600

    [sort] final version

--- sort/main.py
+++ sort/main.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
 
+import os
+import sys
+import io
+import re
+
 def main():
-  print("Hello,world!")  
+  items = []
+  for filename in sys.argv[1:]:
+    if not os.path.exists(filename):
+      print("Warning: %s not found, skip" % filename)
+      continue
+    items += re.findall(r'-?[0-9]+',io.open(filename).read())
+  items.sort(key=int)
+  [print(i) for i in items]
 
 if __name__ == "__main__":
-  main()
+  try:
+    main()
+  except MemoryError as e:
+    print("Error: Out of memory exception")
