60396c363c349c761a1175c28ef999e9f32de7c7
Bogdan Melnik <ld@hackerdom.ru>
2013-01-24 00:22:41 +0600

    Memory pool, bug fixes

--- efi/src/main.c
+++ efi/src/main.c
@@ -3,83 +3,52 @@
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
-    InitializeLib(ImageHandle, SystemTable);
+    EFI_MEMORY_DESCRIPTOR *descriptors;
+    EFI_STATUS status;
+    UINTN neededMemory = 0, actualSize, descriptors_size;
+    UINT64 totalMemory = 0;
 
-    char descriptors[1024 * sizeof(EFI_MEMORY_DESCRIPTOR)];
-    UINTN size = 1024 * sizeof(EFI_MEMORY_DESCRIPTOR);
-    UINTN a,ActualSize;
-    UINT32 c;
-    UINTN status;
+    InitializeLib(ImageHandle, SystemTable);
+    status = uefi_call_wrapper(SystemTable->BootServices->GetMemoryMap, 
+            5, 
+            &neededMemory, NULL, NULL, NULL, NULL);
+    if ( status != EFI_BUFFER_TOO_SMALL )
+    {
+        Print(L"Something going wrong\n");
+        return EFI_SUCCESS;
+    }
+
+    status = uefi_call_wrapper(SystemTable->BootServices->AllocatePool,
+            3, 
+            EfiLoaderData, neededMemory, &descriptors);
+    if (status != EFI_SUCCESS)
+    {
+        Print (L"AllocatePool failed\n");
+        return EFI_SUCCESS;
+    }
 
     status = uefi_call_wrapper(
             SystemTable->BootServices->GetMemoryMap,
             5, 
-            &size, descriptors, &a, &ActualSize, &c);
+            &descriptors_size, descriptors, NULL, &actualSize, NULL);
 
     if ( status == EFI_SUCCESS )
     {
-        Print(L"GetMemoryMap() Success!\n");
-        int i;
-        Print(L"%d %d\n",size,ActualSize);
-        for (i = 0; i < size/ActualSize; ++i)
+        UINTN count = descriptors_size / actualSize;
+        UINTN i;
+        for ( i = 0; i < count; ++i )
         {
-            EFI_MEMORY_DESCRIPTOR descriptor = *((EFI_MEMORY_DESCRIPTOR*) descriptors + i * ActualSize);
-
-            switch (descriptor.Type)
+            if ( descriptors[i].Type == EfiLoaderData ||
+                    descriptors[i].Type == EfiBootServicesData || 
+                    descriptors[i].Type == EfiRuntimeServicesData ||
+                    descriptors[i].Type == EfiConventionalMemory)
             {
-                case EfiReservedMemoryType:
-                    Print(L"EfiReservedMemoryType\n");
-                    break;
-                case EfiLoaderCode:
-                    Print(L"EfiLoaderCode\n");
-                    break;
-                case EfiLoaderData:
-                    Print(L"EfiLoaderData\n");
-                    break;
-                case EfiBootServicesCode:
-                    Print(L"EfiBootServicesCode\n");
-                    break;
-                case EfiBootServicesData:
-                    Print(L"EfiBootServicesData\n");
-                    break;
-                case EfiRuntimeServicesCode:
-                    Print(L"EfiRuntimeServicesCode\n");
-                    break;
-                case EfiRuntimeServicesData:
-                    Print(L"EfiRuntimeServicesData\n");
-                    break;
-                case EfiConventionalMemory:
-                    Print(L"EfiConventionalMemory\n");
-                    break;
-                case EfiUnusableMemory:
-                    Print(L"EfiUnusableMemory\n");
-                    break;
-                case EfiACPIReclaimMemory:
-                    Print(L"EfiACPIReclaimMemory\n");
-                    break;
-                case EfiACPIMemoryNVS:
-                    Print(L"EfiACPIMemoryNVS\n");
-                    break;
-                case EfiMemoryMappedIO:
-                    Print(L"EfiMemoryMappedIO\n");
-                    break;
-                case EfiMemoryMappedIOPortSpace:
-                    Print(L"EfiMemoryMappedIOPortSpace\n");
-                    break;
-                case EfiPalCode:
-                    Print(L"EfiPalCode\n");
-                    break;
-                case EfiMaxMemoryType:
-                    Print(L"EfiMaxMemoryType\n");
-                    break;
-                default:
-                    Print(L"UnknownMemoryType\n");
-                    break;
+                totalMemory += descriptors[i].NumberOfPages * 4096;
             }
-            Print(L"    PhysicalStart:  %X\n", descriptor.PhysicalStart);
-            Print(L"    VirtualStart:   %X\n", descriptor.VirtualStart);
-            Print(L"    NumberOfPages:  %X\n", descriptor.NumberOfPages);
         }
+        Print(L"%d\n", totalMemory);
     }        
+
+    uefi_call_wrapper(SystemTable->BootServices->FreePool, 1, descriptors);
     return EFI_SUCCESS;
 }
